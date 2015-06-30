b1135602b61f83e7b4fd6b4adee4d7d62e197429
Sarah Yu <sarah.m.yu88@gmail.com>
2015-06-30 16:16:48 -0700

    (deploy) changed db config

--- server/config/knex-config.js
+++ server/config/knex-config.js
@@ -5,14 +5,20 @@ knex = require('knex')({
   connection: {
     user: 'root',
     host: '127.0.0.1',
-    database: 'heroku_033c9dc05af850f',
+    database: 'watchly',
     charset: 'utf8'
   }
 });
 } else if (process.env.NODE_ENV === 'production') {
   knex = require('knex')({
     client: 'mysql',
-    connection: process.env.DATABASE_URL
+    connection : {
+       host : 'us-cdbr-iron-east-02.cleardb.net',
+       user : 'b821932131f56c',
+       password : 'ce0d1daf',
+       database : 'heroku_033c9dc05af850f',
+       charset : 'utf8'
+     }
   });
 }
 module.exports = knex; 
