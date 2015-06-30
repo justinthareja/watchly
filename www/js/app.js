dcb23e21503022e2f55c24d4dcb3d3e26d82ff54
m-arnold <michaelaarnold@gmail.com>
2015-06-30 16:27:25 -0700

    (init) refactor services and controllers into separate folders

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,10 @@
-angular.module('watchly', ['ionic', 'watchly.controllers', 'ngCordova'])
+angular.module('watchly', [
+  'ionic',
+  'watchly.MapCtrl', 'watchly.ImageCtrl',
+  'watchly.Auth', 'watchly.FileService', 'watchly.ImageService',
+  'watchly.Incidents', 'watchly.Messages',
+  'ngCordova'
+])
 
 .run(function ($ionicPlatform) {
   $ionicPlatform.ready(function () {
---
+++ www/js/controllers/ImageCtrl.js
@@ -0,0 +1,36 @@
+angular.module('watchly.ImageCtrl', ['watchly.ImageService', 'watchly.FileService'])
+
+.controller('ImageCtrl', function ($scope, $cordovaDevice, $cordovaFile, $ionicPlatform, $cordovaEmailComposer, $ionicActionSheet, ImageService, FileService) {
+
+  $ionicPlatform.ready(function () {
+    $scope.images = FileService.images();
+    $scope.$apply();
+  });
+
+  $scope.urlForImage = function (imageName) {
+    var trueOrigin = cordova.file.dataDirectory + imageName;
+    return trueOrigin;
+  }
+
+  $scope.addMedia = function () {
+    $scope.hideSheet = $ionicActionSheet.show({
+      buttons: [{
+        text: 'Take photo'
+      }, {
+        text: 'Photo from library'
+      }],
+      titleText: 'Add images',
+      cancelText: 'Cancel',
+      buttonClicked: function (index) {
+        $scope.addImage(index);
+      }
+    });
+  }
+
+  $scope.addImage = function (type) {
+    $scope.hideSheet();
+    ImageService.handleMediaDialog(type).then(function () {
+      $scope.$apply();
+    });
+  }
+})
