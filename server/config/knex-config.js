9213bc50495708dda5d89ffe50207dde49235f60
m-arnold <michaelaarnold@gmail.com>
2015-06-30 16:27:25 -0700

    (init) refactor services and controllers into separate folders
    (deploy) modified files for deployment to heroku
    
    (deploy) modified bower
    
    (deploy) specified npm version
    
    (deploy) removed bower install
    
    (deploy) fixed bower.json
    
    (deploy) fixed package.json
    
    (deploy) tracking library files
    
    (deploy) now pushing knex-config file
    
    (deploy) Tried changing sql schema
    
    (deploy) Changed db name

--- server/config/knex-config.js
+++ server/config/knex-config.js
@@ -1,11 +1,18 @@
-var knex = require('knex')({
+var knex;
+if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined) {
+knex = require('knex')({
   client: 'mysql',
   connection: {
     user: 'root',
     host: '127.0.0.1',
-    database: 'watchly',
+    database: 'heroku_033c9dc05af850f',
     charset: 'utf8'
   }
 });
-
-module.exports = knex;
+} else if (process.env.NODE_ENV === 'production') {
+  knex = require('knex')({
+    client: 'mysql',
+    connection: process.env.DATABASE_URL
+  });
+}
+module.exports = knex; 
