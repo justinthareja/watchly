baf846600de3121fddf4ba67c7114e2f6159b232
m-arnold <michaelaarnold@gmail.com>
2015-06-30 20:16:52 -0700

    (feat) user can upload and POST photo

--- www/js/controllers/ImageCtrl.js
+++ www/js/controllers/ImageCtrl.js
@@ -1,36 +1,25 @@
-angular.module('watchly.ImageCtrl', ['watchly.ImageService', 'watchly.FileService'])
+angular.module('watchly.ImageCtrl', [])
 
-.controller('ImageCtrl', function ($scope, $cordovaDevice, $cordovaFile, $ionicPlatform, $cordovaEmailComposer, $ionicActionSheet, ImageService, FileService) {
+.controller('ImageCtrl', function ($scope, $http) {
 
-  $ionicPlatform.ready(function () {
-    $scope.images = FileService.images();
-    $scope.$apply();
-  });
+  $scope.uploadFile = function (files) {
+    var fd = new FormData(files);
+    //Take the first selected file
+    fd.append("file", files[0]);
 
-  $scope.urlForImage = function (imageName) {
-    var trueOrigin = cordova.file.dataDirectory + imageName;
-    return trueOrigin;
-  }
+    $http.post("/api/sign_s3", fd, {
+      withCredentials: true,
+      headers: {
+        'Content-Type': undefined
+      },
+      transformRequest: angular.identity
+    })
+      .success(function () {
+        console.log('success!')
+      })
+      .error(function () {
+        console.log('error!')
+      });
+  };
 
-  $scope.addMedia = function () {
-    $scope.hideSheet = $ionicActionSheet.show({
-      buttons: [{
-        text: 'Take photo'
-      }, {
-        text: 'Photo from library'
-      }],
-      titleText: 'Add images',
-      cancelText: 'Cancel',
-      buttonClicked: function (index) {
-        $scope.addImage(index);
-      }
-    });
-  }
-
-  $scope.addImage = function (type) {
-    $scope.hideSheet();
-    ImageService.handleMediaDialog(type).then(function () {
-      $scope.$apply();
-    });
-  }
 })
